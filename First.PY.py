3d8252f958de6263368ff32eec4d33ec6ef0483a
Syeda Fatima <fatimashah2005@yahoo.com>
2024-11-02 04:06:02 -0700

    Add files via upload

---
+++ First.PY.py
@@ -0,0 +1,39 @@
+number1=6
+number2=1
+number3=8
+
+#calculate sum
+sum=number1+number2+number3
+print("sum:",sum)
+
+#calculate difference
+difference=number1-number2-number3
+print("difference:",difference)
+
+#calculate product
+product=number1*number2*number3
+print("product:",product)
+
+#calculate average
+average=(number1+number2+number3)/3
+print("average:",average)
+
+#area of triangle
+breadth=3
+height=5
+area=0.5*breadth*height
+print("triangle area",area)
+#area of square
+side=3
+area=side*side
+print("square area",area)
+#area of circle
+pi=3.14
+radius=7
+area=pi*radius*radius
+print("circle area",area)
+#rectangle area
+length=6
+width=8
+area=length*width
+print("rectangle area")
